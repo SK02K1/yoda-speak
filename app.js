d236f58be6f2ffe342fa0a139d04ca3b34942ca5
SK02K1 <ksourabh458@gmail.com>
2021-06-19 17:05:02 +0530

    mock url replaced

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const inputContainer = document.querySelector(".input-container");
 const btnTranslate = document.querySelector(".btn-translate");
 const outputContainer = document.querySelector(".output-container");
-const url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+const url = "https://api.funtranslations.com/translate/yoda.json";
 
 const constructUrl = text => `${url}?text=${text}`;
 const errorHandler = error => alert(`${error} (Please try after some time!)`);

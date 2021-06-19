e0d1818c3667d13fb699a79a494d8a3ebcb036e0
SK02K1 <ksourabh458@gmail.com>
2021-06-19 16:56:39 +0530

    mock url added

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const inputContainer = document.querySelector(".input-container");
 const btnTranslate = document.querySelector(".btn-translate");
 const outputContainer = document.querySelector(".output-container");
+const url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
 
-const btnClickHandler = () => outputContainer.innerText = inputContainer.value;
+const constructUrl = text => `${url}?text=${text}`;
+
+const btnClickHandler = () => {
+    fetch(constructUrl(inputContainer.value))
+    .then(Response => Response.json())
+    .then(json => console.log(json.contents.translated))
+}
 
 btnTranslate.addEventListener("click", btnClickHandler)

6b0b9529bd32e8343b505b41ce345f8463ea2f67
SK02K1 <ksourabh458@gmail.com>
2021-06-19 17:02:47 +0530

    input processing output done

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const inputContainer = document.querySelector(".input-container");
 const btnTranslate = document.querySelector(".btn-translate");
 const outputContainer = document.querySelector(".output-container");
-const url = "brokehttps://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+const url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
 
 const constructUrl = text => `${url}?text=${text}`;
 const errorHandler = error => alert(`${error} (Please try after some time!)`);
@@ -9,7 +9,7 @@ const errorHandler = error => alert(`${error} (Please try after some time!)`);
 const btnClickHandler = () => {
     fetch(constructUrl(inputContainer.value))
     .then(Response => Response.json())
-    .then(json => console.log(json.contents.translated))
+    .then(json => outputContainer.innerText = json.contents.translated)
     .catch(errorHandler)
 }
 

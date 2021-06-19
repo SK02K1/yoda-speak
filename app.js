7625fcb667b433ad56c368a73e3968e4de5d9b8a
SK02K1 <ksourabh458@gmail.com>
2021-06-19 16:49:57 +0530

    output container wiring done

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const inputContainer = document.querySelector(".input-container");
 const btnTranslate = document.querySelector(".btn-translate");
+const outputContainer = document.querySelector(".output-container");
 
-const btnClickHandler = () => console.log(inputContainer.value);
+const btnClickHandler = () => outputContainer.innerText = inputContainer.value;
 
 btnTranslate.addEventListener("click", btnClickHandler)
